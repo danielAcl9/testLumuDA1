6d73fc45b4b2832020581c2861c21a5590ee35ae
Daniel Amado <dafeamca99@gmail.com>
2024-04-12 13:12:38 -0500

    Created statistics modules and print

--- main.py
+++ main.py
@@ -45,11 +45,50 @@ def format_data(data):
              "timestamp": convert_to_iso(line_list[0]), # Pos = 0
              "name": line_list[8], # Pos = 8
              "client_ip": line_list[5], # Pos = 5
-            #  "client_name": line_list[8], # Pos = 8
-            #  "type": line_list[10] # Pos = 10
+             "client_name": line_list[8], # Pos = 8
+             "type": line_list[10] # Pos = 10
         }
         return_arr.append(my_object)
     return return_arr
+
+def print_statistics(data):
+    print("\nTotal records: ", len(data))
+    print("\nClient IPs Rank")
+
+    frec_client_ip = {}
+    
+    for obj in data:
+        client_ip = obj['client_ip']
+        frec_client_ip[client_ip] = frec_client_ip.get(client_ip, 0) + 1
+
+    # Ordena el diccionario por frecuencia de mayor a menor
+    frecuencia_client_ip_ordenada = sorted(frec_client_ip.items(), key=lambda x: x[1], reverse=True)
+
+    # Imprime los 5 principales valores y sus frecuencias en columnas
+    print("-" * 40)
+    for client_ip, frecuencia in frecuencia_client_ip_ordenada[:5]:
+        print("{:<20} {:<10} {:<4.2f}%".format(client_ip, frecuencia, (frecuencia / len(data) * 100)))
+
+    print("-" * 40)
+
+    print("\nHost Rank")
+    print("-" * 70)
+
+    frec_name = {}
+    
+    for obj in data:
+        name = obj['name']
+        frec_name[name] = frec_name.get(name, 0) + 1
+
+    # Ordena el diccionario por frecuencia de mayor a menor
+    frecuencia_name_ordenada = sorted(frec_name.items(), key=lambda x: x[1], reverse=True)
+
+    # Imprime los 5 principales valores y sus frecuencias en columnas
+    for name, frecuencia in frecuencia_name_ordenada[:5]:
+        print("{:<50} {:<10} {:<4.2f}%".format(name, frecuencia, (frecuencia / len(data) * 100)))
+
+    print("-" * 70)
+
 
 if __name__ == '__main__':
 
@@ -57,12 +96,13 @@ if __name__ == '__main__':
     data = read_data()
     formatted_data = format_data(data)
 
-    # print(formatted_data)
 
     # Make a POST request to the endpoint
+    # url = 'https://api.lumu.io/collectors/5ab55d08-ae72-4017-a41c-d9d735360288/dns/queries?key=d39a0f19-7278-4a64-a255-b7646d1ace80'
+    # response = requests.post(url, json=formatted_data)
 
-    url = 'https://api.lumu.io/collectors/5ab55d08-ae72-4017-a41c-d9d735360288/dns/queries?key=d39a0f19-7278-4a64-a255-b7646d1ace80'
+    # print(formatted_data[:100])
 
-    x = requests.post(url, json=formatted_data)
 
-    print(x)
+    # Print the statistics
+    print_statistics(formatted_data)
